bf8c3daa157a70946443bda8ba6b408f5d385b6d
Modify24x7 <19856038+Modify24x7@users.noreply.github.com>
2022-08-29 10:37:28 -0700

    Update Test.cpp

--- DotNetCLR/Test.cpp
+++ DotNetCLR/Test.cpp
@@ -9,37 +9,61 @@
 #include <typeinfo>
 #include <mutex>
 
+// Load the assembly from disk
+/*void loadFromDisk(_AppDomainPtr pAppDomainPtr)
+{
+	auto dsdfs = pLoadFromDisk(pAppDomainPtr, L"FileToHex");
+
+	_MethodInfo* dada;
+	dsdfs->get_EntryPoint(&dada);
+
+	variant_t targetObject0;
+	targetObject0.vt = VT_NULL;
+
+	SAFEARRAY* vtArg0 = { };
+	variant_t result0;
+	dada->Invoke_3(targetObject0, vtArg0, &result0);
+}*/
+
 int main(int argc)
 {
 	ICorRuntimeHost* pCorRuntimeHost = NULL;
 
-	//_MethodInfo* mInfo;
 	HRESULT hr;
 
 	// start
 	_AppDomainPtr pAppDomainPtr = pGetAppDomainPtr(L"v4.0.30319", pCorRuntimeHost);
-	if (pAppDomainPtr == NULL) 
+	if (pAppDomainPtr == NULL)
 	{
+		// Release
 		pCleanup(pCorRuntimeHost);
 		return 0;
 	}
-	
+
 	// get mscorlib
 	_AssemblyPtr pAssemblyPtr = pGetCoreLibAssembly(pAppDomainPtr);
 
+	Initialize(pAssemblyPtr);
+
+	// find type
 	_TypePtr pConsole = pFind_Type(pAssemblyPtr, "System.Console");
 	_TypePtr pString = pFind_Type(pAssemblyPtr, "System.String");
 
 	bstr_t mName = L"WriteLine";
 	_MethodInfo* mInfo;
+
+
+	// type
 	IUnknown* typ(pString);
 	variant_t typs[] = { typ };
-	hr = pConsole->GetMethod_5(mName,
-		pToSafeArray(typs, GetArraySize(typs), VT_UNKNOWN), &mInfo);
+
+	// get method info
+	hr = pConsole->GetMethod_5(mName, pToSafeArray(typs, GetArraySize(typs), VT_UNKNOWN), &mInfo);
 	if (FAILED(hr))
 	{
 		wprintf(L"GetMethod_5 failed w/hr 0x%08lx\n", hr);
 
+		// Release
 		pCleanup(pCorRuntimeHost);
 		return 0;
 	}
@@ -48,31 +72,26 @@ int main(int argc)
 	variant_t vtArg[] = { variant_t(L"Hello") };
 	variant_t result;
 
-	hr = mInfo->Invoke_3(targetObject,
-		pToSafeArray(vtArg, GetArraySize(vtArg), VT_VARIANT), &result);
+	// invoke method to write to console
+	hr = mInfo->Invoke_3(targetObject, pToSafeArray(vtArg, GetArraySize(vtArg), VT_VARIANT), &result);
 	if (FAILED(hr))
 	{
 		wprintf(L"Invoke_3 failed w/hr 0x%08lx\n", hr);
 
+		// Release
 		pCleanup(pCorRuntimeHost);
 		return 0;
 	}
 
+	// argument
 	wchar_t* argument = L"World";
 	variant_t vtArg2[] = { argument };
+
+	// invoke method to write to console
 	pInvoke_Public_Static_Method(pConsole, mName, vtArg2, GetArraySize(vtArg2));
 
-	/*// pLoadFromDisk
-	auto dsdfs = pLoadFromDisk(pAppDomainPtr, L"StrongVM.Test");
-	_MethodInfo* dada;
-	dsdfs->get_EntryPoint(&dada);
-
-	variant_t targetObject0;
-	targetObject0.vt = VT_NULL;
-
-	SAFEARRAY *vtArg0 = { };
-	variant_t result0;
-	dada->Invoke_3(targetObject0, vtArg0, &result0); // pLoadFromDisk End*/
+	// Load the assembly from disk
+	// loadFromDisk(pAppDomainPtr);
 
 	// Release
 	pCleanup(pCorRuntimeHost);
