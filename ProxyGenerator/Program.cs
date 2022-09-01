7a458aabe6d6402f8b939d79a853ae5d332eaf48
Modify24x7 <19856038+Modify24x7@users.noreply.github.com>
2022-08-31 21:19:36 -0700

    Added Proxy Generator
    ProxyGenerator Beta v1.0

---
+++ ProxyGenerator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+/// <summary>
+/// Name: Program
+/// Description: Main class
+/// Author: Gorav Gupta
+/// </summary>
+
+namespace ProxyGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 2)
+            {
+                if (args[1].ToLower().Equals("true"))
+                {
+                    ProxyGenerator.AssemblyToHeader(args[0], true);
+                }
+                else if (args[1].ToLower().Equals("false"))
+                {
+                    ProxyGenerator.AssemblyToHeader(args[0], false);
+                }
+            }
+            else
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.AppendLine("ProxyGenerator Beta v1.0");
+                stringBuilder.AppendLine("Description: DotNet Assembly To Cpp Header Generator");
+                stringBuilder.AppendLine("Author: Gorav Gupta");
+                stringBuilder.AppendLine();
+                stringBuilder.AppendLine("ProxyGenerator.exe assembly true");
+                stringBuilder.AppendLine("ProxyGenerator.exe assembly false");
+                stringBuilder.AppendLine();
+                stringBuilder.AppendLine("true: generate type, methodinfo method in header file, compile time appear error, fix manually.");
+                stringBuilder.AppendLine();
+                stringBuilder.Append("false: generate type in header file");
+
+                Console.WriteLine(stringBuilder.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ProxyGenerator/RandomNum.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+/// <summary>
+/// Name: RandomHelper, Random, RandomNum
+/// Snip From ConfuserEx
+/// </summary>
+
+namespace ProxyGenerator
+{
+    public class RandomHelper
+    {
+        public static RandomNum Random { get; set; }
+
+        static RandomHelper()
+        {
+            Random = new RandomNum();
+        }
+    }
+
+    public class Random
+    {
+        public static string GetRandomString(int len)
+        {
+            char[] dictionary = "ABCEDFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray();
+
+            string shit = "";
+            for (int i = 0; i < len; i++)
+            {
+                shit += dictionary[RandomHelper.Random.Next(dictionary.Length)];
+            }
+            return "s" + shit.ToLower();
+        }
+    }
+
+    public class RandomNum : RandomNumberGenerator
+    {
+        private static RandomNumberGenerator r;
+
+        public RandomNum()
+        {
+            r = Create();
+        }
+
+        public override void GetBytes(byte[] buffer)
+        {
+            r.GetBytes(buffer);
+        }
+
+        public double NextDouble()
+        {
+            byte[] b = new byte[4];
+            r.GetBytes(b);
+            return (double)BitConverter.ToUInt32(b, 0) / uint.MaxValue;
+        }
+
+        public int Next(int minValue, int maxValue)
+        {
+            return (int)Math.Floor(NextDouble() * (maxValue - minValue)) + minValue;
+        }
+
+        public int Next()
+        {
+            return Next(0, int.MaxValue);
+        }
+
+        public int Next(int maxValue)
+        {
+            return Next(0, maxValue);
+        }
+
+        public byte NextByte()
+        {
+            return (byte)Next(byte.MaxValue);
+        }
+
+        public uint NextUInt32()
+        {
+            return (uint)Next() * 2;
+        }
+
+        public void Shuffle<T>(IList<T> list)
+        {
+            for (int i = list.Count - 1; i > 1; i--)
+            {
+                int k = Next(i + 1);
+                T tmp = list[k];
+                list[k] = list[i];
+                list[i] = tmp;
+            }
+        }
+
+        public override void GetNonZeroBytes(byte[] data)
+        {
+            r.GetNonZeroBytes(data);
+        }
+    }
+}
